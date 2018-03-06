2d7b8a7377604c789d9390a8ea7eaf053e6fa270
Will Hawks <will@creativedistillery.com>
2018-03-06 11:47:16 -0600

    added columns to regular content

--- template-parts/regular_content.php
+++ template-parts/regular_content.php
@@ -1,17 +1,42 @@
-
+<?php
+  $cols = get_sub_field( 'columns' );
+?>
 
 <div class="regular-content"<?php if ( get_sub_field( 'regular_content_background_color' ) ) : ?> style="background-color: <?php the_sub_field( 'regular_content_background_color' ); ?>"<?php endif; ?>>
 
-  <h2 class="regular-content__title<?php if ( get_sub_field( 'regular_content_background_color' ) ) : ?>--color-white<?php endif; ?>">
-    <span class="line"></span>
-    <span class="text">
-      <?php the_sub_field( 'title' ); ?>
-    </span>
-    <span class="line"></span>
-  </h2>
+  <?php if ( get_sub_field( 'title' ) ) : ?>
+    <h2 class="regular-content__title<?php if ( get_sub_field( 'regular_content_background_color' ) ) : ?>--color-white<?php endif; ?>">
+      <span class="line"></span>
+      <span class="text">
+        <?php the_sub_field( 'title' ); ?>
+      </span>
+      <span class="line"></span>
+    </h2>
+  <?php endif; ?>
 
-  <div class="regular-content__content<?php if ( get_sub_field( 'regular_content_background_color' ) ) : ?>--color-white<?php endif; ?>">
-    <?php the_sub_field( 'content' ); ?>
-  </div>
+  <?php if ( $cols > 1) : ?>
+    <div class="regular-content__row<?php if ( get_sub_field( 'regular_content_background_color' ) ) : ?>--color-white<?php endif; ?>">
 
+      <div class="regular-content__content--<?php echo $cols; ?>-cols">
+        <?php the_sub_field( 'content' ); ?>
+      </div>
+      <div class="regular-content__content--<?php echo $cols; ?>-cols">
+        <?php the_sub_field( 'content_2' ); ?>
+      </div>
+      <?php if ( $cols > 2 ) : ?>
+        <div class="regular-content__content--<?php echo $cols; ?>-cols">
+          <?php the_sub_field( 'content_3' ); ?>
+        </div>
+        <?php if ( $cols == 4 ) : ?>
+          <div class="regular-content__content--<?php echo $cols; ?>-cols">
+            <?php the_sub_field( 'content_4' ); ?>
+          </div>
+        <?php endif; ?>
+      <?php endif; ?>
+    </div>
+  <?php else: ?>
+    <div class="regular-content__content<?php if ( get_sub_field( 'regular_content_background_color' ) ) : ?>--color-white<?php endif; ?>">
+      <?php the_sub_field( 'content' ); ?>
+    </div>
+  <?php endif; ?>
 </div>
