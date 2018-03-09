c0268041cda4c179580372aababf0b16aa2e11e7
Will Hawks <will@creativedistillery.com>
2018-03-09 15:49:19 -0600

    added cta contact row component

---
+++ template-parts/cta_contact_row.php
@@ -0,0 +1,12 @@
+<div class="cta-contact-row">
+
+<div class="cta-contact-row__content">
+  <img class="cta-contact-row__image" src="<?php echo get_template_directory_uri(); ?>/assets/img/star.svg" />
+
+  <a class="cta-contact-row__link" href="<?php the_sub_field( 'link' ); ?>">
+    <?php the_sub_field( 'text' ); ?>  
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/arrow.svg" />
+  </a>
+</div>
+
+</div>
