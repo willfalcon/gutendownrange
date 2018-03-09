75490dd6eb3200e90b16bb3c3e31973807a90899
Will Hawks <will@creativedistillery.com>
2018-03-09 15:12:04 -0600

    added cta button and subscribe form components

---
+++ template-parts/cta_button.php
@@ -0,0 +1,9 @@
+<?php $link = get_sub_field( 'button' ); ?>
+
+<div class="cta-button">
+
+  <a class="button" href="<?php echo $link['url']; ?>" target="<?php echo $link['target']; ?>">
+    <?php echo $link['title']; ?>
+  </a>
+
+</div>
---
+++ template-parts/subscribe_form.php
@@ -0,0 +1,7 @@
+<div class="subscribe-form">
+  
+  <h3 class="subscribe-form__title"><?php the_sub_field( 'title' ); ?></h3>
+
+  <?php gravity_form( get_sub_field( 'form' ), false, false, false, null, true ); ?>
+
+</div>
