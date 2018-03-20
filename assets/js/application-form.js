2287e173bad49f07b381f68bb848150b008c7755
Will Hawks <will@creativedistillery.com>
2018-03-20 17:06:17 -0500

    fixed endpoint

--- assets/js/application-form.js
+++ assets/js/application-form.js
@@ -41,7 +41,7 @@ if (appForm) {
     // get the value of the age group selection (the id of the tax term)
     const termId = e.target.value;
     // get all the camp dates
-    const endpoint = 'http://localhost/campdownrange.dev/wp-json/wp/v2/camp_dates?filter[camp_type]=' + appFor;
+    const endpoint = '../wp-json/wp/v2/camp_dates?filter[camp_type]=' + appFor;
     console.log(endpoint);
     fetch(endpoint)
       .then(response => response.json())
