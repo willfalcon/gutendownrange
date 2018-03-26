7ea043be4cdd5e197b999da881a96c7ed6a88f7b
Will Hawks <will@creativedistillery.com>
2018-03-26 14:35:11 -0500

    switch out donate background image with flat topped one

--- page.php
+++ page.php
@@ -1,16 +1,17 @@
-<?php get_header(); ?>
 <?php
+
+  get_header();
 
   if ( have_posts() ) : while ( have_posts() ) : the_post();
 
     if ( have_rows( 'page_content' ) ) : while ( have_rows( 'page_content' ) ) : the_row();
 
       get_template_part( 'template-parts/' . get_row_layout() );
-      
+
     endwhile; endif;
 
   endwhile; endif;
 
-?>
+  get_footer();
 
-<?php get_footer(); ?>
+?>
