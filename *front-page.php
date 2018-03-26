914dc3ea0f1adb0d9ae7bf577b8b176c54c4d642
Will Hawks <will@creativedistillery.com>
2018-03-26 15:45:29 -0500

    delete front-page.php

--- *front-page.php
+++
@@ -1,11 +0,0 @@
-<?php get_header(); ?>
-
-  <div class="top">
-    <div class="top__content slide-down on-load">
-      <p class="font-main">Camp Down Range creates challenging experiences to push participants past their comfort  zones and gain a lifelong benefit  of team bonding, personal development, and the ability  of triumph over adversity.</p>
-    </div>
-  </div>
-
-  <?php get_template_part( 'template-parts/donate' ); ?>
-
-<?php get_footer(); ?>
